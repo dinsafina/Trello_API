5cfd2e60bca45ed5b277dc36341f333c92af0fb9
Dinara Safina <dinakama94@gmail.com>
2023-05-29 19:02:25 +0300

    Properties modified

--- src/main/java/property/PropertyVars.java
+++ src/main/java/property/PropertyVars.java
@@ -7,21 +7,20 @@ import java.util.Properties;
 public class PropertyVars {
     public static final String PATH_TO_PROPERTIES = "variables.properties";
 
-public static Values getProperty(){
-    String url;
-    String key;
-    String token;
-    Properties prop = new Properties();
+    public static Values getProperty() {
+        String url;
+        String key;
+        String token;
+        Properties prop = new Properties();
 
-    try (InputStream in = PropertyVars.class.getClassLoader().getResourceAsStream(PATH_TO_PROPERTIES)) {
-        prop.load(in);
-    } catch (IOException ex) {
-        System.out.println("Ошибка в программе: файл " + PATH_TO_PROPERTIES + " не обнаружено");
+        try (InputStream in = PropertyVars.class.getClassLoader().getResourceAsStream(PATH_TO_PROPERTIES)) {
+            prop.load(in);
+        } catch (IOException ex) {
+            System.out.println("Ошибка в программе: файл " + PATH_TO_PROPERTIES + " не обнаружено");
+        }
+        url = prop.getProperty("trello_url");
+        key = prop.getProperty("key");
+        token = prop.getProperty("token");
+        return new Values(url, key, token);
     }
-    url = prop.getProperty("trello_url");
-    key = prop.getProperty("key");
-    token = prop.getProperty("token");
-    return new Values(url, key, token);
-}
-
 }
--- src/main/java/property/Values.java
+++ src/main/java/property/Values.java
@@ -1,9 +1,9 @@
 package property;
 
 public class Values {
-    private String url;
-    private String user_key;
-    private String user_token;
+    private static String url;
+    private static String user_key;
+    private static String user_token;
 
     public Values(String url, String user_key, String user_token) {
         this.url = url;
@@ -11,16 +11,16 @@ public class Values {
         this.user_token = user_token;
     }
 
-    public String getUrl() {
+    public static String getUrl() {
         return url;
     }
 
-    public String getUser_key() {
+    public static String getUser_key() {
         return user_key;
     }
 
 
-    public String getUser_token() {
+    public static String getUser_token() {
         return user_token;
     }
 }
